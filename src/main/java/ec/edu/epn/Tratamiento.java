990dc3ddb92609e1bdaa3d4008dc313c98c78baa
Alexis Vizuete <50114642+alexisvc@users.noreply.github.com>
2022-06-21 21:29:24 -0500

    Creación de métodos de las clases diagnostico, endodoncia, periodoncia, ortodoncia y lista tratamientos.

--- src/main/java/ec/edu/epn/Tratamiento.java
+++ src/main/java/ec/edu/epn/Tratamiento.java
@@ -1,4 +1,12 @@
 package ec.edu.epn;
 
 public interface Tratamiento {
+    public abstract String registrarTratamiento(String id, String procedimiento, String prescripcion);
+
+    public abstract Boolean existeTratamiento(String id, String fecha);
+
+    public abstract String actualizarTratamiento(String id, String nuevoProcedimiento, String fecha);
+
+    public abstract String actualizarPrescripcionTratamiento(String id, String nuevoPrescripcion, String fecha);
+
 }
